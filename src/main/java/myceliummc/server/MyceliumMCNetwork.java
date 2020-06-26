35763d0e86a7f392e77f7fd72a27a5a94b7f16d0
蕭澧邦 <45505768+shou692199@users.noreply.github.com>
2020-06-26 20:00:50 +0800

    Create MyceliumMCNetwork.java

---
+++ src/main/java/myceliummc/server/MyceliumMCNetwork.java
@@ -0,0 +1,33 @@
+package MyceliumMC.server;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.network.ForgeMessage;
+import net.minecraftforge.common.network.ForgeNetworkHandler;
+import net.minecraftforge.fml.common.network.FMLEmbeddedChannel;
+import net.minecraftforge.fml.common.network.FMLOutboundHandler;
+
+import java.util.Map;
+
+public class MyceliumMCNetwork {
+    public static boolean isSendDataSerializers(Map<String, String> modList) {
+        String forgeVersion = modList.get("forge");
+        if (forgeVersion != null) {
+            try {
+                if (Integer.parseInt(forgeVersion.split("\\.")[3]) < 27) {
+                    return false;
+                }
+            } catch (Exception ignored) {}
+        }
+        return true;
+    }
+
+    public static void registerBukkitWorldToClient(EntityPlayerMP player, int dimension) {
+        if (DimensionManager.isBukkitDimension(dimension)) {
+            FMLEmbeddedChannel serverChannel = ForgeNetworkHandler.getServerChannel();
+            serverChannel.attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
+            serverChannel.attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(player);
+            serverChannel.writeOutbound(new ForgeMessage.DimensionRegisterMessage(dimension, DimensionManager.getProviderType(dimension).name()));
+        }
+    }
+}
